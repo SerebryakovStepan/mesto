6b65bbbe47dce6e6e98199c71c843be79c453808
SerebryakovStepan <magnum44916@gmail.com>
2021-08-23 20:06:39 +0300

    important fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,23 @@
 let openPopupButton = document.querySelector(".profile__edit-profile");
 let popup = document.querySelector(".popup");
 let popupCloseButton = document.querySelector(".popup__close-button");
-let formFieldset = document.querySelector(".form__fieldset");
 let userName = document.querySelector(".profile__name");
 let aboutUser = document.querySelector(".profile__description");
 let nameInput = document.querySelector("#profile-name");
 let jobInput = document.querySelector("#profile-caption");
 let form = document.querySelector(".form");
-let formSaveButton = document.querySelector(".form__save-button");
 
 function togglePopup() {
-  if (!popup.classList.contains("popup_opened")) {
+  if (!popup.classList.contains("popup_is-opened")) {
     nameInput.value = userName.textContent;
     jobInput.value = aboutUser.textContent;
   }
   popup.classList.toggle("popup_is-opened");
 }
 
-openPopupButton.addEventListener("click", togglePopup);
-popupCloseButton.addEventListener("click", togglePopup);
+popupCloseButton.addEventListener("click", function () {
+  togglePopup();
+});
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -26,9 +25,8 @@ form.addEventListener("submit", function (event) {
   userName.textContent = nameInput.value;
   aboutUser.textContent = jobInput.value;
 
-  popup.classList.toggle("popup_is-opened");
-
+  togglePopup();
 });
 
-popupCloseButton.addEventListener('click', function () {togglePopup();});
-
+openPopupButton.addEventListener("click", togglePopup);
+popupCloseButton.addEventListener("click", togglePopup);
